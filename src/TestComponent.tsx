ca9de51be229eef6b7252fe4be1165c3f5367dd3
ryuya416 <h2luv0r1a8@icloud.com>
2021-10-19 21:46:21 +0900

    ✨ useState

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   text: string;
 }
+interface UserData {
+  id: number;
+  name: string;
+}
 
 export const TestComponent: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number | null>(null);
+  const [user, setUser] = useState<UserData>({ id: 1, name: "yamada" });
+
   return (
     <div>
       <h1>{props.text}</h1>
+      {count}
+      {user.name}
     </div>
   );
 };
