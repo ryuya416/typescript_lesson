76815bd1b7e6dadcf2093a847f6e93fa80f8c669
ryuya416 <h2luv0r1a8@icloud.com>
2021-10-19 18:47:11 +0900

    ✨ typeof

--- src/App.tsx
+++ src/App.tsx
@@ -53,6 +53,20 @@ value = 10;
 let arrayUni: (number | string)[];
 arrayUni = [0, 1, 2, "hello"];
 
+let company: "Facebook" | "Google" | "Amazon";
+company = "Amazon";
+
+let memory: 256 | 512;
+memory = 256;
+
+//typeof
+let msg: string = "Hi";
+let msg2: typeof msg;
+msg2 = "hello";
+
+let animal = { cat: "small cat" };
+let newAnimal: typeof animal = { cat: "big cat" };
+
 function App() {
   return (
     <div className="App">
