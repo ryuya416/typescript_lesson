cf6450ec9a6b9556d2aec33229e123a5da5483f5
ryuya416 <h2luv0r1a8@icloud.com>
2021-10-19 21:38:00 +0900

    ✨ React Hooks Props型

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import "./App.css";
 import Data from "./data.json";
+import { TestComponent } from "./TestComponent";
 
 type USERS = typeof Data;
 
@@ -160,12 +161,14 @@ const funcGen4 = <T extends Props>(props: T) => {
   return { value: props.price };
 };
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <TestComponent text="hello from App" />
+      </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TestComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  text: string;
+}
+
+export const TestComponent: React.FC<Props> = (props) => {
+  return (
+    <div>
+      <h1>{props.text}</h1>
+    </div>
+  );
+};
